ee1f65b90a21c60bc1edbddf5ffada175d689a50
vietdoo <dodoviet@icloud.com>
2023-10-06 01:00:11 +0700

    hide maps

--- components/map.tsx
+++ components/map.tsx
@@ -1,24 +1,24 @@
-"use client";
+// "use client";
 
-import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
-import "leaflet-defaulticon-compatibility";
+// import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
+// import 'leaflet/dist/leaflet.css'
+// import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
+// import "leaflet-defaulticon-compatibility";
 
-const Map = () => {
-  return (
-    <MapContainer >
-      <Marker 
-      position={[40.8054,-74.0241]}
-    //   draggable={true}
-    //   animate={true}
-      >
-        <Popup>
-          Hey ! you found me
-        </Popup>
-      </Marker>
-    </MapContainer>
-  )
-}
+// const Map = () => {
+//   return (
+//     <MapContainer >
+//       <Marker 
+//       position={[40.8054,-74.0241]}
+//     //   draggable={true}
+//     //   animate={true}
+//       >
+//         <Popup>
+//           Hey ! you found me
+//         </Popup>
+//       </Marker>
+//     </MapContainer>
+//   )
+// }
 
-export default Map
+// export default Map
