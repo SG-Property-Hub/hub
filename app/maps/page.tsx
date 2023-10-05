c1ec595795c3c5cecc9cdc12d47090ff28196a28
vietdoo <dodoviet@icloud.com>
2023-10-06 01:01:33 +0700

    hide maps

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -1,14 +1,14 @@
-import Map from '@components/map'
+// import Map from '@components/map'
 
-export default async function Maps() {
+// export default async function Maps() {
 
   
-  return (
-    <div>
-        <h1 className="flex">Maps</h1>
-        <Map />
-    </div>
+//   return (
+//     <div>
+//         <h1 className="flex">Maps</h1>
+//         <Map />
+//     </div>
     
-  )
-}
+//   )
+// }
 
