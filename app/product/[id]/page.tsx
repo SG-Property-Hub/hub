7eec5823ffe86f5765a5942f4e018dfeb7e1b613
vietdoo <dodoviet@icloud.com>
2023-10-06 01:18:31 +0700

    related products

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,7 +4,7 @@
 import { useState } from "react";
 import HouseDetails from "@components/HouseDetails";
 import { HouseCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
-import { fetchHouse } from "@utils";
+import { fetchHouse, fetchHouses } from "@utils";
 import Link from 'next/link';
 import { Suspense } from 'react';
 
@@ -79,7 +79,7 @@ async function RelatedProducts({ id }: { id: string }) {
             key={product.title}
             className="aspect-square w-full flex-none min-[475px]:w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5"
           >
-            <Link className="relative h-full w-full" href={`/product/${product.title}`}>
+            <Link className="relative h-full w-full" href={`/product/${product.raw_id}`}>
               <GridTileImage
                 alt={product.title}
                 label={{
@@ -87,7 +87,7 @@ async function RelatedProducts({ id }: { id: string }) {
                   amount: product.price.toString(),
                   currencyCode: 'VND'
                 }}
-                src={'https://cdn.chotot.com/XI28WDFwhRO7cWRHD75RFFpTY-8qIQDwwgHe_sBN3gw/preset:view/plain/2fa2de440c0ffca408c3c41ea9d1e437-2846090137772897146.jpg'}
+                src={product.img}
                 fill
                 sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
               />
